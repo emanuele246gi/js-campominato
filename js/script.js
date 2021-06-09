e7744904181d6018878798d3395364af5e4bb6ca
Emanuele <emanuele65b@gmail.com>
2021-06-09 15:08:30 +0200

    add array vuoto

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 // ------------------------------------
 
 // 1. 16 numeri random tra 1 e 100 - no duplicati
-var random = i
+var random = []
 for (var i = 0; i < 16; i ++){
     random.push = Math.floor(Math.random() * 100 + 1);
 
